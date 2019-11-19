c9105cdc99b9428a93ea170a58e04944f79c92d6
Shigeru Hagiwara <hgwrsgr@gmail.com>
2019-11-19 17:09:56 +0900

    delete all files from git

--- routes/newmemo.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-// Newmemoにgetでアクセスしてきたときの処理//
-router.get("/", function(req, res) {
-  res.render("newmemo", { title: "メモアプリ" });
-});
-
-module.exports = router;
